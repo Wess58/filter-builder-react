9c7da831c42c27bf17669e778fde007bf90454f4
wess58 <wesleymbate@gmail.com>
2025-09-22 22:19:47 +0200

    setup project file-structure and basic components

---
+++ src/lib/components/FilterBuilder.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function FilterBuilder() {
+
+  return (
+    <div>
+      <p>FilterBuilder placeholder </p>
+    </div>
+  );
+}
---
+++ src/lib/index.tsx
@@ -0,0 +1,2 @@
+export { FilterBuilder } from "./components/FilterBuilder";
+// export type { FilterJSON } from "./types";
