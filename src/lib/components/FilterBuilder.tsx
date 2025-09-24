8ae38b83780cbfaf663a4e7f77bf18d2a937da95
wess58 <wesleymbate@gmail.com>
2025-09-24 11:35:29 +0200

    created recursive filter UI with groups and conditions added schema & operators as props to ensure the builder is not static added a live JSON preview

--- src/lib/components/FilterBuilder.tsx
+++ src/lib/components/FilterBuilder.tsx
@@ -1,10 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export function FilterBuilder() {
+import {
+  type ConditionNode,
+  type GroupNode,
+  type OperatorsMap,
+  type Schema,
+  type ApiConfig,
+  generateId,
+} from "../constants/types";
+
+import { Group } from "./GroupNodeComponent";
+import { deserialize, serialize } from "../utils/serialization";
+import { validateGroup } from "../utils/validation-group";
+import { sendFilters } from "../utils/api-config";
+
+
+type Props = {
+  schema: Schema;
+  operators: OperatorsMap;
+  initialJson?: any;
+  onChange: (json: any) => void;
+};
+
+export function FilterBuilder({
+  schema,
+  operators,
+  initialJson,
+  onChange,
+}: Props) {
+  const [tree, setTree] = useState<GroupNode>(
+    initialJson
+      ? deserialize(initialJson, generateId)
+      : {
+          id: generateId(),
+          type: "group",
+          operator: "and",
+          children: [],
+        }
+  );
+
+  useEffect(() => {
+    if (onChange) {
+      const validation = validateGroup(tree);
+      // onChange(serialize(tree));
+
+      validation.valid
+        ? onChange(serialize(tree))
+        : onChange({ error: validation.error });
+    }
+  
+  }, [tree]);
 
   return (
     <div>
-      <p>FilterBuilder placeholder </p>
+      <Group
+        group={tree}
+        schema={schema}
+        operators={operators}
+        onChange={setTree}
+      />
     </div>
   );
 }
