1519f00303d4c1df6e957a0f9f44eb04e357bb9f
wess58 <wesleymbate@gmail.com>
2025-09-24 11:36:45 +0200

    added GET / POST API intergration surpport

---
+++ src/lib/utils/api-config.ts
@@ -0,0 +1,26 @@
+import type { GroupNode } from "../constants/types";
+import { serialize } from "./serialization";
+
+export async function sendFilters(
+	tree: GroupNode,
+	config: { mode: "GET" | "POST"; url: string; queryParam?: string }
+) {
+	const json = serialize(tree);
+
+	if (config.mode === "GET") {
+		const queryString = encodeURIComponent(JSON.stringify(json));
+		const url = `${config.url}?${config.queryParam}=${queryString}`;
+
+		const res = await fetch(url, { method: "GET" });
+		return res.json();
+	}
+
+	if (config.mode === "POST") {
+		const res = await fetch(config.url, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(json),
+		});
+		return res.json();
+	}
+}
